ebf05585fbbafe5bda77a19985a5181c91098fde
nataliedutz <natalie.dutz@dci-student.org>
2024-03-18 11:05:41 +0100

    Delete laptop shop related code

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,11 +1,1 @@
 from django.urls import path
-from .views import BrandListCreateAPIView, BrandRetrieveUpdateDestroyAPIView, LaptopListCreateAPIView, LaptopRetrieveUpdateDestroyAPIView, LaptopShopView, LaptopShopDetailView
-
-urlpatterns = [
-    path('brands/', BrandListCreateAPIView.as_view(), name='brand_list'),
-    path('brands/<int:pk>/', BrandRetrieveUpdateDestroyAPIView.as_view(), name='brand_detail'),
-    path('laptops/', LaptopListCreateAPIView.as_view(), name='laptop_list'),
-    path('laptops/<int:pk>/', LaptopRetrieveUpdateDestroyAPIView.as_view(), name='laptop_detail'),
-    path('laptop-shop/', LaptopShopView.as_view(), name='laptop_shop'),
-    path('laptop-shop/<int:pk>/', LaptopShopDetailView.as_view(), name='laptop_shop_detail'),
-]
